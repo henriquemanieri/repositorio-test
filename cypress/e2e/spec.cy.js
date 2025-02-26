16a58665c179b908be0655ea16c036511e388d01
henriquemanieri <henrique.manieri@hotmail.com>
2025-02-26 17:47:48 -0500

    test

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -4,6 +4,6 @@ describe('template spec', () => {
     cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Admin')
     cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('admin123')
     cy.get('.oxd-button').click()
-    cy.get('.oxd-topbar-header-breadcrumb > .oxd-text').contains('Dashboard')
+
   })
 })
